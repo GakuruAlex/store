11f7a3fd44e568427cc3d2a72cfe7e6290093540
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-12 14:04:37 +0300

    execute postgres query for current time

--- app/controllers/server_time_controller.rb
+++ app/controllers/server_time_controller.rb
@@ -1,6 +1,6 @@
 class ServerTimeController < ApplicationController
   def db_time
-    @db_time = ActiveRecord::Base.connection.execute("SELECT CURRENT_TIMESTAMP;")[0]["CURRENT_TIMESTAMP"]
+    @db_time= ActiveRecord::Base.connection.execute("SELECT NOW();").values[0][0]
     @server_time = Time.now
 
     render :server_time
