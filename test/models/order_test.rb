5c1b620dd50d22784e0fdad9746a9d380dde76a2
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-17 21:55:03 +0300

    tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id               :integer          not null, primary key
+#  amount           :decimal(, )
+#  created_at       :datetime         not null
+#  customer_id      :integer
+#  date_of_purchase :date
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_orders_on_customer_id  (customer_id)
+#
+
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  category   :string
+#  created_at :datetime         not null
+#  name       :string
+#  price      :float
+#  quantity   :integer
+#  updated_at :datetime         not null
+#
+
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
