05f5957fa2ca3e27cbf1d5bc73688a55969d708a
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-18 22:41:41 +0300

    Index action for Orders and order_params created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :new]
   def index
     @orders = Order.includes(:customer)
   end
 
   def show
   end
+  def new
 
+  end
   def edit
   end
 
   def update
+  end
+
+  private
+  def order_params
+    params.expect(order: [:date_of_purchase, :amount, order_products_attributes: [[:product_id, :quantity]]])
+  end
+
+  def set_order
+    @order = Order.find_by(params[:id])
   end
 end
