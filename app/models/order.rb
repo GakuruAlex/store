eb533ed897da17a0e61944df63915c80a2e4bfb7
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-18 22:43:13 +0300

    Accept nested attributes for order product in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -18,4 +18,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products
   has_many :products, through: :order_products
+  accepts_nested_attributes_for :order_products
 end
