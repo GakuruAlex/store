5d5a7ce48f78f23f28f35db1412a2fb18e6ca728
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-23 00:09:52 +0300

    Add destroy to before action for set order and add destroy action for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [ :show, :edit, :update ]
+  before_action :set_order, only: [ :show, :edit, :update , :destroy]
   def index
     @orders = Order.includes(:customer)
   end
@@ -22,6 +22,10 @@ class OrdersController < ApplicationController
   end
 
   def update
+  end
+
+  def destroy
+    @order.destroy
   end
 
   private
